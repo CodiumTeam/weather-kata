e41eed022d6b42de78f5badb6cb32204a55eeec5
Luis Rovirosa <luisrovirosa@gmail.com>
2018-07-06 09:12:10 +0200

    adds csharp code

---
+++ csharp/WeatherKata/ForecastService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Team.Codium.LegacyTraining.WeatherKata
+{
+    interface ForecastService
+    {
+        string PredictWeather(string cityName, DateTime when);
+
+        string predictWind(string cityName, DateTime when);
+    }
+}
